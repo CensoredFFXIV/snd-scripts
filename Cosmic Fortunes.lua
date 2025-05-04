d864e9cf49028c59cb99e8975cea1e992d657336
CensoredFFXIV <atzendj@gmail.com>
2025-05-04 17:32:59 +0200

    Create Cosmic Fortunes.lua
    First Draft

---
+++ Cosmic Fortunes.lua
@@ -0,0 +1,137 @@
+local itemsWanted = {
+    ["Vacuum Suit Identification Key"] = 200,
+    ["Cosmosuit Coffer"] = 50,
+    ["Micro Rover"] = 25,
+    ["Ballroom Etiquette - Personal Per..."] = 25, --Ballroom Etiquette - Personal Perfection
+    ["Loparasol"] = 5,
+    ["The Faces We Wear - Tinted Sungl..."] = 5,   --The Faces We Wear - Tinted Sunglasses
+    ["Verdant Partition"] = 5,
+    ["Stellar Opportunity"] = 0,
+    ["Metallic Cobalt Green Dye"] = 0,
+    ["Metallic Dark Blue Dye"] = 0,
+    ["Metallic Pink Dye"] = 0,
+    ["Metallic Ruby Red Dye"] = 0,
+    ["Cracked Prismaticrystal"] = 0,
+    ["Cracked Novacrystal"] = 0,
+    ["Echoes in the Distance Orchestri..."] = 0, --Echoes in the Distance Orchestrion Roll
+    ["Close in the Distance (Instrumental) Orchestrion Roll"] = 0,
+    ["Stargazers Orchestrion Roll"] = 0,
+    ["Crafter's Delineation"] = 0,
+    ["Drafting Table"] = 0,
+    ["Cosmotable"] = 0,
+    ["Cosmolamp"] = 0,
+    ["Cordial "] = 0,
+    ["Magicked Prism (Cosmic Explorat..."] = 0 --Magicked Prism (Cosmic Exploration)
+}
+local npc = { name = "Orbitingway", x = 17, y = -1, z = -16 }
+local itemsInFirstWheel = {}
+local itemsInSecondWheel = {}
+
+local function calculateTotalWeight(itemsInWheel)
+    local totalWeight = 0
+
+    for itemName, count in pairs(itemsInWheel) do
+        local itemWeight = itemsWanted[itemName]
+
+        if itemWeight and itemWeight > 0 then
+            totalWeight = totalWeight + (itemWeight * count)
+        end
+    end
+
+    return totalWeight
+end
+
+local function getNames()
+    for i = 1, 7 do
+        if IsNodeVisible("WKSLottery", 1, 30, 38 - i) then
+            local itemName = GetNodeText("WKSLottery", 29 + i, 6)
+
+            if itemsInFirstWheel[itemName] then
+                itemsInFirstWheel[itemName] = itemsInFirstWheel[itemName] + 1
+            else
+                itemsInFirstWheel[itemName] = 1
+            end
+        end
+
+        if IsNodeVisible("WKSLottery", 1, 40, 48 - i) then
+            local itemName = GetNodeText("WKSLottery", 19 + i, 6)
+
+            if itemsInSecondWheel[itemName] then
+                itemsInSecondWheel[itemName] = itemsInSecondWheel[itemName] + 1
+            else
+                itemsInSecondWheel[itemName] = 1
+            end
+        end
+    end
+end
+
+if not IsAddonVisible("WKSLottery") then
+    -- Thanks to pot0to for some of this script
+
+    repeat
+        if not PathfindInProgress() and not PathIsRunning() then
+            PathfindAndMoveTo(npc.x, npc.y, npc.z)
+        end
+
+        yield("/wait 1")
+    until GetDistanceToPoint(npc.x, npc.y, npc.z) > 0
+
+    if PathfindInProgress() or PathIsRunning() then
+        yield("/vnav stop")
+    end
+
+    yield("/target " .. npc.name)
+    yield("/wait 0.5")
+    yield("/interact")
+
+    repeat
+        yield("/wait 0.5")
+    until IsAddonVisible("SelectString")
+
+    yield("/wait 0.5")
+    yield("/callback SelectString true 0")
+    yield("/wait 1")
+    yield("/callback SelectString true 0")
+end
+
+while GetItemCount(45691) >= 1000 do
+    if IsAddonVisible("SelectYesno") then
+        yield("/callback SelectYesno true 0")
+    end
+
+    yield("/wait 1")
+
+    itemsInFirstWheel = {}
+    itemsInSecondWheel = {}
+
+    getNames()
+
+    local totalWeightFirstWheel = calculateTotalWeight(itemsInFirstWheel)
+    local totalWeightSecondWheel = calculateTotalWeight(itemsInSecondWheel)
+
+    yield("/callback WKSLottery true 0 0")
+
+    if totalWeightFirstWheel > totalWeightSecondWheel then
+        yield("First wheel is better with total weight: " .. totalWeightFirstWheel)
+        yield("/callback WKSLottery true 1 0")
+    elseif totalWeightSecondWheel > totalWeightFirstWheel then
+        yield("Second wheel is better with total weight: " .. totalWeightSecondWheel)
+        yield("/callback WKSLottery true 1 1")
+    else
+        yield("Both wheels are equal in weight.")
+        yield("/callback WKSLottery true 1 0")
+    end
+
+    yield("/wait 1")
+    yield("/callback WKSLottery true 2 0")
+    yield("/wait 1")
+
+    if IsNodeVisible("WKSLottery", 1, 27) then
+        yield("Stellar Fortune, needs manual")
+
+        repeat
+            yield("/wait 1")
+        until not IsNodeVisible("WKSLottery", 1, 27)
+        -- yield("/callback WKSLottery true 2 0")
+    end
+end
